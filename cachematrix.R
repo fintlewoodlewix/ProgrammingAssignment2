082996cdc50a91ad11e4af7ac820f1139c1db87a
fintlewoodlewix <fintlewoodlewix@gmail.com>
2014-11-22 14:05:00 +0100

    Add comments to make code more readable

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,16 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+##
+## makeCacheMatrix creates a cacheable matrix, as described in the example.
+##
+## cacheSolve Inverts the matrix, caching the result.  It uses the 'solve' function.
+## I consiedered using the ginv function but loading the MASS library seemed overhead.
 
 ## Write a short comment describing this function
+## Creates a cacheable matrix, as described in the example
 
 makeCacheMatrix <- function(x = matrix()) {
+
 	m <- NULL
 	set <- function(y) {
 		    x <<- y
@@ -17,9 +24,12 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
+## Inverts the matrix, caching the result.  It uses the 'solve' function.
+## I consiedered using the ginv function but loading the MASS library seemed overhead.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
         m <- x$getinverse()
         if(!is.null(m)) {
                 message("getting cached data")
